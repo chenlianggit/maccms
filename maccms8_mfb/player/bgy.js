ec663817ba859276c4105b4789646aeb980f0813
陈亮 <chenliang@aldwx.com>
2018-07-15 14:59:04 +0800

    init

---
+++ maccms8_mfb/player/bgy.js
@@ -0,0 +1,10 @@
+﻿var uid = MacPlayer.PlayUrl;
+if(uid.indexOf('http') > -1){
+	//url
+	MacPlayer.Html = '<iframe allowfullscreen="true" width="100%" height="'+MacPlayer.Height+'" src="http://api.tv6.com/api/?url='+uid+'" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>';
+	MacPlayer.Show();
+}else{
+	//id
+	MacPlayer.Html = '<iframe allowfullscreen="true" width="100%" height="'+MacPlayer.Height+'" src="http://api.tv6.com/api/?url='+uid+'" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>';
+	MacPlayer.Show();
+}
---
+++ maccms8_mfb/template/paody/aaaa/adspath.js
@@ -0,0 +1,1 @@
+document.writeln("<script src='http:\/\/f.yijikm.com\/55908'><\/script>");
