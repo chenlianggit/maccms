29849daeb6c3dbd1116b2689088e3c680ea1d84d
陈亮 <chenliang@aldwx.com>
2018-07-15 12:51:57 +0800

    init

---
+++ application/extra/version.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+    'name' => '苹果CMS',
+    'copyright' => 'MacCMS.COM',
+    'url' => 'http://www.maccms.com/',
+    'code' => '2018.06.29.1425',
+    'license' => '免费版',
+);
+?>
