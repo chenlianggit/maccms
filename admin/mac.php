16c9cce423734e8d690b42fd59288be5cdf9342c
陈亮 <chenliang@aldwx.com>
2018-07-15 15:54:33 +0800

    采集

---
+++ admin/mac.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=7" />
+<title>资源采集管理</title>
+<script type="text/javascript" src="../js/jquery.js"></script>
+<script type="text/javascript" src="../js/jq/jquery.cookie.js"></script>
+<script type="text/javascript" src="../js/jq/jquery.validate.js"></script>
+<script type="text/javascript" src="tpl/js/adm.js"></script>
+<style type='text/css'>.table {margin-bottom:5px;font-size:12px;text-align:left;border-collapse: collapse;border: 1px solid #C6DCF2; }.table td {padding-left:10px;padding-right:10px;border: 1px solid #C6DCF2; }.table .td {height:25px;padding-left:0px;} .table tr {height:25px;line-height:25px;}.table tr .left{width:180px;text-align:right;padding-right:10px;color:#666;} .table_title {height:25px;line-height:25px;background: #F3F7FB;font-weight:bold;color:#0099cc;}.uok{width:30px;height:15px;color:white;background-color:green;display:-moz-inline-box;display:inline-block;}.uerr{width:30px;height:15px;color:white;background-color:red;display:-moz-inline-box;display:inline-block;}.red{color:red;}</style>
+<script language="javascript">
+$(function(){
+	$("#xmllist").html( $("#xml").html());
+	$("#xml").html("");
+});
+</script>
+</head>
+<body>
+<!--请勿擅自修改本页内容，若修改后出现无法采集的现象，后果自负！-->
+<table width="100%" border="0" cellspacing="0" cellpadding="0" >
+<tr>
+<td><span id="xmllist">资源列表载入中……</span></td>
+</tr>
+</table>
+<script language="javascript">
+document.write("<script type=\"text/javascript\" src=\"http://www.zidongcaiji.com/caiji/mac\"><\/script>");
+</script>
+</body>
+</html>
