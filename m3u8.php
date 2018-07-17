0d060d146cdd16a8e125871f9bc1f309bc1e18dc
陈亮 <chenliang@aldwx.com>
2018-07-17 23:07:58 +0800

    mym3u8

--- m3u8.php
+++ m3u8.php
@@ -8,8 +8,8 @@
 <meta content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" id="viewport" name="viewport">
 </head>
 <body marginwidth="0" marginheight="0" style="position:absolute;width:100%;top:0;bottom:0;backgroung:#000">
-<link rel="stylesheet" href="./DPlayer.min.css">
-<script src="./jquery.min.js"></script>
+<link rel="stylesheet" href="/js/m3u8/DPlayer.min.css">
+<script src="/js/m3u8/jquery.min.js"></script>
 <div id="player1"></div>
 <script type="text/javascript" src="/js/m3u8/hls.min.js"></script>
 <script type="text/javascript" src="/js/m3u8/DPlayer.min.js" charset="utf-8"></script>
