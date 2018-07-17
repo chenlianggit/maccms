3ca8af061de8f98a65d2afe0152d0ea25f917b7d
陈亮 <chenliang@aldwx.com>
2018-07-17 23:25:08 +0800

    m3u8验证

--- m3u8.php
+++ m3u8.php
@@ -9,6 +9,9 @@
 </head>
 <body marginwidth="0" marginheight="0" style="position:absolute;width:100%;top:0;bottom:0;backgroung:#000">
 <link rel="stylesheet" href="/js/m3u8/DPlayer.min.css">
+<?php if( isset($_GET['url']) || !$_GET['url']){
+        echo '请输入m3u8视频格式地址';exit;
+} ;?>
 <script src="/js/m3u8/jquery.min.js"></script>
 <div id="player1"></div>
 <script type="text/javascript" src="/js/m3u8/hls.min.js"></script>
