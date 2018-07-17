e25c473202d75061f547d012585495c0ebf8aea5
陈亮 <chenliang@aldwx.com>
2018-07-17 10:50:30 +0800

    play1

--- player/bgy.js
+++ player/bgy.js
@@ -1,10 +1,2 @@
-﻿var uid = MacPlayer.PlayUrl;
-if(uid.indexOf('http') > -1){
-	//url
-	MacPlayer.Html = '<iframe allowfullscreen="true" width="100%" height="'+MacPlayer.Height+'" src="http://api.tv6.com/api/?url='+uid+'" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>';
-	MacPlayer.Show();
-}else{
-	//id
-	MacPlayer.Html = '<iframe allowfullscreen="true" width="100%" height="'+MacPlayer.Height+'" src="http://api.tv6.com/api/?url='+uid+'" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>';
-	MacPlayer.Show();
-}
+﻿MacPlayer.Html = '<iframe width="100%" height="'+MacPlayer.Height+'" src="http://206dy.com/vip.php?url='+MacPlayer.PlayUrl+'" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>';
+MacPlayer.Show();
