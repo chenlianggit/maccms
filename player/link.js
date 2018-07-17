ac9a61dbec5d63fdc2edfc0457598c63df41e07a
陈亮 <chenliang@aldwx.com>
2018-07-17 23:22:52 +0800

    都换成我的m3u8

--- player/link.js
+++ player/link.js
@@ -1,2 +1,2 @@
-MacPlayer.Html = '<iframe width="100%" height="'+MacPlayer.Height+'" src="http://206dy.com/vip.php?url='+MacPlayer.PlayUrl+'" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>';
+MacPlayer.Html = '<iframe width="100%" height="'+MacPlayer.Height+'" src="http://206dy.com/vip.php?url='+MacPlayer.PlayUrl+'&type=lemid" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>';
 MacPlayer.Show();
