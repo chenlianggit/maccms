3a74d24e2dd35fd66572726585587e8284ddd0e3
陈亮 <chenliang@aldwx.com>
2018-07-19 22:27:55 +0800

    joeleo

---
+++ template/Joeleo/ads/yingshi.js
@@ -0,0 +1,2 @@
+<a href="/?m=vod-type-id-4.html">
+							<img src="http://ubmcmm.baidustatic.com/media/v1/0f000Q1WFCGXL_oG3jw036.jpg" />
---
+++ template/Joeleo/html/label_tv1.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="Cache-Control" content="no-siteapp" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="renderer" content="webkit">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no,minimal-ui">
+	<title>电视直播</title>
+	<meta name="keywords" content="{maccms:keywords}" />
+    <meta name="description" content="{maccms:description}" />
+   	<link rel="stylesheet" href="/template/Joeleo/css/aui.css" />
+	<link rel="stylesheet" href="/template/Joeleo/css/swiper.min.css" />
+	<link rel="stylesheet" href="/template/Joeleo/css/main.css" />
+	<script type="text/javascript" src="/template/Joeleo/css/iconfont.js" ></script>
+    <script>var SitePath='{maccms:path}',SiteAid='{maccms:siteaid}',SiteTid='{maccms:sitetid}',SiteId='{maccms:siteid}';</script>
+        <style type="text/css">
+    .icon {
+     width: 1em; height: 0.8em;
+     font-size: 1.6rem;
+     vertical-align: -0.15em;
+     fill: currentColor;
+     overflow: hidden;
+}
+    .icono {
+     width: 1em; height: 0.8em;
+     font-size: 1.6rem;
+      color:#212121;
+     vertical-align: -0.15em;
+     fill: currentColor;
+     overflow: hidden;
+}
+</style>
+ 
+
+</head>
+<body oncontextmenu=self.event.returnValue=false onselectstart="return false">
+    
+<!-- 头部  -->   
+<section class="header">
+	<header class="aui-bar aui-bar-nav" id="aui-header">
+		<font style="float: left;margin-left: 10px;">Joeleo</font>
+	    <a href="/" class="aui-pull-right aui-btn" aui-popup-for="top-right">
+			 <svg class="icono" aria-hidden="true">
+             <use xlink:href="#icon-imac1"></use>
+        </svg>
+		</a>
+	    <a onclick="openSearch()" class="aui-pull-right aui-btn">
+	        <svg class="icono" aria-hidden="true">
+             <use xlink:href="#icon-search"></use>
+        </svg>
+	    </a>
+	</header>
+</section>
+<section class="header-nav">
+	<div class="nav-wrap">
+		<div class="shadow-l"></div>
+		<div class="shadow-r"></div>
+	    <div class="wrap">
+			<ul>
+	   			<li><a href="/template/Joeleo/html/label_tv.php">央视频道</a></li>
+                <li class="active"><a href="/template/Joeleo/html/label_tv1.php">卫视频道</a></li>
+                <li><a href="/template/Joeleo/html/label_tv2.php">地方频道</a></li>
+               
+			</ul>
+		</div>
+	</div>
+	
+	<div class="nav-wrapp">
+	    
+		<div class="more">
+		    <a href="" rel="nofollow" ><svg class="icon" aria-hidden="true">
+             <use xlink:href="#icon-list"></use>
+        </svg></a>
+		</div>
+	</div>
+</section>
+
+<!-- 直播开始  --> 
+<section class="aui-content">
+
+<section class="col-head"style="margin-top: 2.2rem;">
+		<div class="title"> 
+		<svg class="icon" aria-hidden="true">
+           <use xlink:href="#icon-fanjutuijian"></use>
+        </svg>卫视频道</div>
+		<div class="more"><i class="aui-iconfont aui-icon-paper"></i>&nbsp好看的电视直播&nbsp</div>
+</section>
+<section class="col-main">
+		<div class="row">	
+            <ul class="mainmenu">
+                <li><a href="/tv/php/0hun.php" ><b><img src="/tv/images/hn.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>湖南卫视</span></a></li>
+                <li><a href="/tv/php/0zj.php" ><b><img src="/tv/images/zj.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>浙江卫视</span></a></li>
+                <li><a href="/tv/php/0js.php" ><b><img src="/tv/images/js.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>江苏卫视</span></a></li>
+                <li><a href="/tv/php/0df.php" ><b><img src="/tv/images/df.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>东方卫视</span></a></li>
+                <li><a href="/tv/php/0bj.php" ><b><img src="/tv/images/bj.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>北京卫视</span></a></li>
+                <li><a href="/tv/php/0ah.php" ><b><img src="/tv/images/ah.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>安徽卫视</span></a></li>
+                <li><a href="/tv/php/0sd.php" ><b><img src="/tv/images/sd.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>山东卫视</span></a></li>
+                <li><a href="/tv/php/0hen.php" ><b><img src="/tv/images/ha.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>河南卫视</span></a></li>
+                <li><a href="/tv/php/0jx.php" ><b><img src="/tv/images/jx.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>江西卫视</span></a></li>
+		        <li><a href="/tv/php/0hlj.php" ><b><img src="/tv/images/hlj.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>黑龙江卫视</span></a></li>
+                <li><a href="/tv/php/0gd.php" ><b><img src="/tv/images/gd.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>广东卫视</span></a></li>
+                <li><a href="/tv/php/0tj.php" ><b><img src="/tv/images/tj.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>天津卫视</span></a></li>
+	            <li><a href="/tv/php/0hub.php" ><b><img src="/tv/images/hub.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>湖北卫视</span></a></li>
+                <li><a href="/tv/php/0heb.php" ><b><img src="/tv/images/heb.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>河北卫视</span></a></li>
+                <li><a href="/tv/php/0cq.php" ><b><img src="/tv/images/cq.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>重庆卫视</span></a></li>
+                <li><a href="/tv/php/0dn.php" ><b><img src="/tv/images/dn.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>东南卫视</span></a></li>
+                <li><a href="/tv/php/0sx1.php" ><b><img src="/tv/images/sx1.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>山西卫视</span></a></li>
+                <li><a href="/tv/php/0gx.php" ><b><img src="/tv/images/gx.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>广西卫视</span></a></li>
+                <li><a href="/tv/php/0gz.php" ><b><img src="/tv/images/gz.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>贵州卫视</span></a></li>
+                <li><a href="/tv/php/0sz.php" ><b><img src="/tv/images/sz.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>深圳卫视</span></a></li>
+                <li><a href="/tv/php/0jl.php" ><b><img src="/tv/images/jl.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>吉林卫视</span></a></li>
+                <li><a href="/tv/php/0ln.php" ><b><img src="/tv/images/ln.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>辽宁卫视</span></a></li>
+                <li><a href="/tv/php/0sc.php" ><b><img src="/tv/images/sc.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>四川卫视</span></a></li>
+               <li><a href="/tv/php/chc.php" ><b><img src="/tv/images/chc.png" style= "height: 1.8rem ;width: 2.8rem;"/></b><span>CHC电影</span></a></li>  
+            </ul>
+ <br> 
+        </div>
+</section>
+</section>
+
+
+<!-- 特别尾部  -->
+<br><br>
+<footer class="aui-bar aui-bar-tab" id="footer">
+    <div class="aui-bar-tab-item" tapmode>
+        <a href="/" rel="nofollow" ><svg class="icon" aria-hidden="true">
+             <use xlink:href="#icon-imac1"></use>
+        </svg></a>
+        <div class="aui-bar-tab-label">首页</div>
+    </div>
+    <div class="aui-bar-tab-item aui-active" tapmode>
+       <a href="" rel="nofollow" ><svg class="icon" aria-hidden="true">
+             <use xlink:href="#icon-radio"></use>
+        </svg></a>
+        <div class="aui-bar-tab-label">直播</div>
+    </div>
+   <div class="aui-bar-tab-item" tapmode>
+        <a href="/music/" rel="nofollow" ><svg class="icon" aria-hidden="true">
+             <use xlink:href="#icon-bird1"></use>
+        </svg></a>
+        <div class="aui-bar-tab-label">音乐</div>
+    </div>
+    <div class="aui-bar-tab-item" tapmode>
+         <a href="/index.php?m=user-index.html" rel="nofollow" ><svg class="icon" aria-hidden="true">
+             <use xlink:href="#icon-avatar"></use>
+        </svg></a>
+        <div class="aui-bar-tab-label">我的</div>
+    </div>
+</footer>
+
+
+
+<script type="text/javascript" src="/template/Joeleo/js/aui-dialog.js" ></script>
+<script type="text/javascript">
+    var dialog = new auiDialog({})
+    function openDialog(type){
+        switch (type) {
+            case "text":
+                dialog.alert({
+                    title:"即将添加地方频道",
+                    msg:'祝您观影愉快！',
+                    buttons:['取消','确定']
+                },function(ret){
+                    console.log(ret)
+                })
+                break;
+        }
+    }
+</script>
+<!-- 尾部  -->
+
+<script type="text/javascript" src="/template/Joeleo/js/zepto.min.js"></script>
+<script type="text/javascript" src="/template/Joeleo/js/common.js" ></script>
+<script type="text/javascript" src="/template/Joeleo/js/home.js" ></script>
+<script type="text/javascript" src="/template/Joeleo/js/aui-scroll.js" ></script>
+<script type="text/javascript" src="/template/Joeleo/js/main.js" ></script>
+
+
+
+<div id="search" class="search">
+	<form class="form" action="/index.php?m=vod-search" method="post">
+	
+		<input type="search" class="input" placeholder="想要搜点什么呢？" name="wd"  autocomplete="off">
+		<button type="submit" class="aui-iconfont aui-icon-search"></button>
+	</form>
+	<div onclick="closeSearch()" class="search-cancel">取消</div>
+</div>
+
+<div id="_js_goTop" class="gotop">
+	<i class="aui-iconfont aui-icon-top"></i>
+</div>
+	
+<script type="text/javascript">
+    /*导航条*/
+    var topNav = new Swiper('#header-nav',{freeMode : true,slidesPerView : 'auto', });
+    /*图片懒加载初始化*/
+    Echo.init({
+		offset: 0,
+		throttle: 0
+	});
+	/*影片列表TAB初始化*/
+	var tabsSwiper = new Swiper('#tabs-container',{
+	    speed:500,
+	    onSlideChangeStart: function(swiper){
+		    $(".numBox .hd .active").removeClass('active');
+		    $(".numBox .hd li").eq(swiper.activeIndex).addClass('active');
+	    }
+	})
+	$(".numBox .hd li").on('touchstart mousedown',function(e){
+	    e.preventDefault()
+	    $(".numBox .hd .active").removeClass('active')
+	    $(this).addClass('active')
+	    tabsSwiper.slideTo( $(this).index() )
+	})
+	$(".numBox .hd li").click(function(e){
+    	e.preventDefault()
+  	});
+  	function closeTips(){
+		$("#tips-1").remove();
+	}
+</script>
+
+
+
+</div>
+</body>
+</html>
