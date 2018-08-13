232e67877197baab35b516ec761d9a06bbf37019
陈亮 <chenliang@aldwx.com>
2018-08-13 22:33:55 +0800

    player zheng

--- player/2mm.js
+++ player/2mm.js
@@ -1,2 +1,2 @@
-MacPlayer.Html = '<iframe width="100%" height="100%" allowTransparency="true" frameborder="0" scrolling="no" src="http://www.q2017.com/xfsub/?url='+MacPlayer.PlayUrl+'"></iframe>';
+MacPlayer.Html = '<iframe width="100%" height="100%" allowTransparency="true" frameborder="0" scrolling="no" src="/vip/?url='+MacPlayer.PlayUrl+'"></iframe>';
 MacPlayer.Show();
