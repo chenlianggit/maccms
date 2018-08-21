f398218dc4741cdd5282db8c81ed5fae85d2fb3a
陈亮 <chenliang@aldwx.com>
2018-08-21 23:58:37 +0800

    修改图

--- tu.php
+++ tu.php
@@ -1,13 +1,10 @@
 <?php
 header("Content-Type: image/jpeg;text/html; charset=utf-8");
 $url = 'http://'.$_GET['tu'];
-$ch = curl_init($url); //       问题反馈：995161098
-//curl_setopt($ch, CURLOPT_REFERER, "http://www.iqiyi.com/"); //伪造来路页面
-//curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-FORWARDED-FOR:'. getIP().'', 'CLIENT-IP:'. getIP().'')); //构造IP 
-curl_setopt($ch, CURLOPT_HEADER, 0); //不返回header部分
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //返回字符串，而非直接输出
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $FH= curl_exec($ch);
 curl_close($ch);
 echo $FH;
 exit; 
-?>
